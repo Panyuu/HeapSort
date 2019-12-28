a01a39a0070a52c5fd1688f1bec93c385c6f05bf
Leon Portius <Pluto-is-not-too-small@gmx.de>
2019-12-28 20:00:25 +0100

    script runs in unity and prints sorted heap

--- HeapSort/Assets/Scripts/MaxHeap.cs
+++ HeapSort/Assets/Scripts/MaxHeap.cs
@@ -4,151 +4,169 @@ using UnityEngine;
 
 public class MaxHeap : MonoBehaviour {
 
-    public class HeapSort {
 
-        /* Ablauf des Algorithmus:
-         * 1. Max-Heap bilden -> Vaterknoten >= Kindknoten && linker Kindknoten >= rechter Kindknoten (von Blatt zur Wurzel vorarbeiten)
-         * 2. letztes Element speichern, Wurzelwert an letzte Stelle schreiben, dann von oben nach unten gehen und immer größten Wert nach oben schreiben
-         * und an unterster Stelle Element wieder einfügen
-         * 3. Max-Heap bilden (von eingefügtem Element nach oben arbeiten, Rest ist schon sortiert)
-         * 4. Wiederhole 2. und 3., bis ganzes Array sortiert ist (Größe des Heaps = 0)
-         */
 
-        public static int[] arrayToSort;
-        public static int arrayLength, root;
+    /* Ablauf des Algorithmus:
+     * 1. Max-Heap bilden -> Vaterknoten >= Kindknoten && linker Kindknoten >= rechter Kindknoten (von Blatt zur Wurzel vorarbeiten)
+     * 2. letztes Element speichern, Wurzelwert an letzte Stelle schreiben, dann von oben nach unten gehen und immer größten Wert nach oben schreiben
+     * und an unterster Stelle Element wieder einfügen
+     * 3. Max-Heap bilden (von eingefügtem Element nach oben arbeiten, Rest ist schon sortiert)
+     * 4. Wiederhole 2. und 3., bis ganzes Array sortiert ist (Größe des Heaps = 0)
+     */
 
-        // Start is called before the first frame update
-        public void Start() {
+    public static int[] arrayToSort;
+    public static int arrayLength, root;
 
-            createArray(new int[] { 10, 20, 5, 14, 7, 3, 1, 9, 24 });
-        }
+    // Start is called before the first frame update
+    public void Start()
+    {
 
-        public void Update() {
+        createArray(new int[] { 10, 20, 5, 14, 7, 3, 1, 9, 24 });
 
-            Debug.Log("Hallo");
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                Debug.Log("Unsorted Array: " + arrayToString());
+    }
 
-                buildHeap();
+    public void Update()
+    {
 
-                while (arrayLength > 0) {
+       // Debug.Log("Hallo");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Unsorted Array: " + arrayToString());
 
-                    arrayLength--;
-                    // Letztes Element merken (vorerst nicht im Array enthalten) und Wurzelelement (größtes Element) an letzte Stelle schreiben.
-                    // -> an Wurzelstelle wird Platz frei
-                    int lastLeaf = arrayToSort[arrayLength];
-                    arrayToSort[arrayLength] = arrayToSort[root];
-                    // rückt Elemente nach und speichert sich Ort der freien Stelle (letzter freier Platz im übrigen Array).
-                    int free = downHeap(root);
-                    // letztes Element, das vorher aus Array gelöscht wurde, wird wieder eingefügt und Heap-Eigenschaft wird wieder geprüft (von unten nach oben).
-                    upHeap(free, lastLeaf);
-                }
+            buildHeap();
 
-                Debug.Log("Sorted Array: " + arrayToString());
-            }
-        }
+            while (arrayLength > 0)
+            {
 
-        // Inizialisierung des zu sortierenden Arrays, sowie Speicherung von dessen Länge.
-        public static void createArray(int[] array) {
-
-            arrayToSort = array;
-            arrayLength = arrayToSort.Length;
-            root = 0;
-        }
-
-        // 1. Max-Heap bilden -> ersten nicht Blattknoten suchen, dann Kinder untersuchen, ggf. tauschen. 
-        // dann zum nächsten Vaterknoten übergehen.
-        public static void buildHeap() {
-
-            for (int parent = arrayLength / 2 - 1; parent >= 0; parent--) {
-
-                heapify(parent);
-                //System.out.println(arrayToString());
-            }
-        }
-
-        // Elemente im Array in Heapstruktur bringen.
-        public static void heapify(int parent) {
-
-            int child = parent * 2 + 1;
-
-            // Solange Kindknoten existieren ...
-            while (child < arrayLength) {
-
-                //System.out.println("parent: " + arrayToSort[parent] + " kind1: " + arrayToSort[child] + " kind2: " + arrayToSort[child + 1] + " ArrayLänge: " + arrayLength);
-                // Wenn es rechten Kindknoten gibt und dieses größer ist als das linke, dann wird dieses weiter betrachtet.
-                if (child + 1 < arrayLength) if (arrayToSort[child + 1] > arrayToSort[child]) child++;
-
-                // Wenn Kind größer ist als Elternknoten, dann Positionswechsel
-                if (arrayToSort[parent] >= arrayToSort[child]) return;
-
-                changePosition(child, parent);
-                parent = child;
-                child = parent * 2 + 1;
-            }
-        }
-
-        // Methode beginnt bei der Wurzel und tauscht das größte Kind (rechts, wenn vorhanden) nach oben an die freie Stelle.
-        // Dabei entsteht an einem der unteren Knoten eine freie Stelle, die später befüllt wird.
-        public static int downHeap(int parent) {
-
-            int child = parent * 2 + 1;
-
-            // Da rechtes Kind größer ist als linkes, wird bevorzugt dieses betrachtet.
-            while (child + 1 < arrayLength) {
-
-                if (arrayToSort[child + 1] > arrayToSort[child]) child++;
-
-                arrayToSort[parent] = arrayToSort[child];
-                parent = child;
-                child = parent * 2 + 1;
-            }
-            // Fall, dass kein rechtes aber ein linkes Kind existiert.
-            if (child < arrayLength) {
-
-                arrayToSort[parent] = arrayToSort[child];
-                parent = child;
+                arrayLength--;
+                // Letztes Element merken (vorerst nicht im Array enthalten) und Wurzelelement (größtes Element) an letzte Stelle schreiben.
+                // -> an Wurzelstelle wird Platz frei
+                int lastLeaf = arrayToSort[arrayLength];
+                arrayToSort[arrayLength] = arrayToSort[root];
+                // rückt Elemente nach und speichert sich Ort der freien Stelle (letzter freier Platz im übrigen Array).
+                int free = downHeap(root);
+                // letztes Element, das vorher aus Array gelöscht wurde, wird wieder eingefügt und Heap-Eigenschaft wird wieder geprüft (von unten nach oben).
+                upHeap(free, lastLeaf);
             }
 
-            return parent;
-        }
-
-        // Fügt fehlendes Element an freier Stelle ein und sortiert dann von unten nach oben, bis Max-Heap-Struktur wieder eingehalten.
-        public static void upHeap(int child, int missingElement) {
-
-            int parent;
-            arrayToSort[child] = missingElement;
-
-            while (child > root) {
-
-                parent = (child - 1) / 2;
-                if (arrayToSort[parent] >= arrayToSort[child]) return;
-
-                changePosition(parent, child);
-                child = parent;
-            }
-        }
-
-        // Tauscht Wert an Index-Stelle a und b miteinander.
-        public static void changePosition(int a, int b) {
-
-            int help = arrayToSort[a];
-            arrayToSort[a] = arrayToSort[b];
-            arrayToSort[b] = help;
-        }
-
-        public static string arrayToString() {
-
-            string array = "";
-
-            for (int i = 0; i < arrayToSort.Length; i++) {
-
-                array += arrayToSort[i] + ", ";
-            }
-
-            return array;
+            Debug.Log("Sorted Array: " + arrayToString());
         }
     }
+
+    // Inizialisierung des zu sortierenden Arrays, sowie Speicherung von dessen Länge.
+    public static void createArray(int[] array)
+    {
+
+        arrayToSort = array;
+        arrayLength = arrayToSort.Length;
+        root = 0;
+    }
+
+    // 1. Max-Heap bilden -> ersten nicht Blattknoten suchen, dann Kinder untersuchen, ggf. tauschen. 
+    // dann zum nächsten Vaterknoten übergehen.
+    public static void buildHeap()
+    {
+
+        for (int parent = arrayLength / 2 - 1; parent >= 0; parent--)
+        {
+
+            heapify(parent);
+            print(arrayToString());
+        }
+    }
+
+    // Elemente im Array in Heapstruktur bringen.
+    public static void heapify(int parent)
+    {
+
+        int child = parent * 2 + 1;
+
+        // Solange Kindknoten existieren ...
+        while (child < arrayLength)
+        {
+
+            print("parent: " + arrayToSort[parent] + " kind1: " + arrayToSort[child] + " kind2: " + arrayToSort[child + 1] + " ArrayLänge: " + arrayLength);
+            // Wenn es rechten Kindknoten gibt und dieses größer ist als das linke, dann wird dieses weiter betrachtet.
+            if (child + 1 < arrayLength) if (arrayToSort[child + 1] > arrayToSort[child]) child++;
+
+            // Wenn Kind größer ist als Elternknoten, dann Positionswechsel
+            if (arrayToSort[parent] >= arrayToSort[child]) return;
+
+            changePosition(child, parent);
+            parent = child;
+            child = parent * 2 + 1;
+        }
+    }
+
+    // Methode beginnt bei der Wurzel und tauscht das größte Kind (rechts, wenn vorhanden) nach oben an die freie Stelle.
+    // Dabei entsteht an einem der unteren Knoten eine freie Stelle, die später befüllt wird.
+    public static int downHeap(int parent)
+    {
+
+        int child = parent * 2 + 1;
+
+        // Da rechtes Kind größer ist als linkes, wird bevorzugt dieses betrachtet.
+        while (child + 1 < arrayLength)
+        {
+
+            if (arrayToSort[child + 1] > arrayToSort[child]) child++;
+
+            arrayToSort[parent] = arrayToSort[child];
+            parent = child;
+            child = parent * 2 + 1;
+        }
+        // Fall, dass kein rechtes aber ein linkes Kind existiert.
+        if (child < arrayLength)
+        {
+
+            arrayToSort[parent] = arrayToSort[child];
+            parent = child;
+        }
+
+        return parent;
+    }
+
+    // Fügt fehlendes Element an freier Stelle ein und sortiert dann von unten nach oben, bis Max-Heap-Struktur wieder eingehalten.
+    public static void upHeap(int child, int missingElement)
+    {
+
+        int parent;
+        arrayToSort[child] = missingElement;
+
+        while (child > root)
+        {
+
+            parent = (child - 1) / 2;
+            if (arrayToSort[parent] >= arrayToSort[child]) return;
+
+            changePosition(parent, child);
+            child = parent;
+        }
+    }
+
+    // Tauscht Wert an Index-Stelle a und b miteinander.
+    public static void changePosition(int a, int b)
+    {
+
+        int help = arrayToSort[a];
+        arrayToSort[a] = arrayToSort[b];
+        arrayToSort[b] = help;
+    }
+
+    public static string arrayToString()
+    {
+
+        string array = "";
+
+        for (int i = 0; i < arrayToSort.Length; i++)
+        {
+
+            array += arrayToSort[i] + ", ";
+        }
+
+        return array;
+    }
+
 
 
 }
