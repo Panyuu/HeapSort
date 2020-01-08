18147623ed6ca6705e27c04bd0f4152a2304a5e7
Chesra <59623653+Chesra@users.noreply.github.com>
2020-01-08 12:05:25 +0100

    Five Sounds on Ship Prefab
    Five Sounds on Ship Prefab

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -6,11 +6,13 @@ using UnityEngine.Audio;
 public class AudioManager : MonoBehaviour
 {
 
+
     public Sounds[] sounds;
 
     // Start is called before the first frame update
     void Awake()
     {
+
         foreach (Sounds s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -18,9 +20,10 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+           
         }
-
-
+        sounds[0].source.loop = true;
+        sounds[1].source.loop = true;
     }
 
    
@@ -30,6 +33,14 @@ public class AudioManager : MonoBehaviour
         s.source.Play();
     }
 
+    private void Start()
+    {
+        Play("music");
+        Play("waves");
+    }
 
-
+    public static void playThisNow(Sounds s)
+    {
+        s.source.Play();
+    }
 }
---
+++ Assets/Scripts/AnimationSounds.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AnimationSounds : MonoBehaviour
+{
+    [SerializeField] Sounds[] animationSound;
+
+    void playMove1()
+    {
+        AudioManager.playThisNow(animationSound[0]);
+    }
+
+    void playMove2()
+    {
+        AudioManager.playThisNow(animationSound[1]);
+    }
+
+    void playMove3()
+    {
+        AudioManager.playThisNow(animationSound[2]);
+    }
+
+    void playMove4()
+    {
+        AudioManager.playThisNow(animationSound[3]);
+    }
+
+    void playMove5()
+    {
+        AudioManager.playThisNow(animationSound[4]);
+    }
+
+    void playCursor1()
+    {
+        AudioManager.playThisNow(animationSound[5]);
+    }
+
+    void playCursor2()
+    {
+        AudioManager.playThisNow(animationSound[6]);
+    }
+
+    void playCorrect1()
+    {
+        AudioManager.playThisNow(animationSound[7]);
+    }
+
+    void playCorrect2()
+    {
+        AudioManager.playThisNow(animationSound[8]);
+    }
+
+    void playFalse1()
+    {
+        AudioManager.playThisNow(animationSound[9]);
+    }
+
+    void playFalse2()
+    {
+        AudioManager.playThisNow(animationSound[10]);
+    }
+
+    void playApplause()
+    {
+        AudioManager.playThisNow(animationSound[11]);
+    }
+}
