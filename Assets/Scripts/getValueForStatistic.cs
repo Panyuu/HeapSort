b263a8a2a99e21c0bcad867b4f384111b54986c5
Leon Portius <Pluto-is-not-too-small@gmx.de>
2020-01-14 21:11:30 +0100

    work on statistics

--- Assets/Scripts/getValueForStatistic.cs
+++ Assets/Scripts/getValueForStatistic.cs
@@ -7,8 +7,9 @@ public class getValueForStatistic : MonoBehaviour
 {
     public static getValueForStatistic GVFS;
 
-    [SerializeField] Text labelComparisons, labelSwitches, labelTimeDelta;
-
+    [SerializeField] Text labelComparisons, labelSwitches, labelTimeDelta, labelBestCaseJudge;
+    // for prefab
+    [SerializeField] GameObject original;
     static int comparison = 0;
     static int switches = 0;
     static float startTime = 0;
@@ -18,8 +19,10 @@ public class getValueForStatistic : MonoBehaviour
     public static int Switches { get => switches; set => switches = value; }
     public static float StartTime { get => startTime; set => startTime = value; }
     public static float EndTime { get => endTime; set => endTime = value; }
+    public static List<int> OwnArr { get => ownArr; set => ownArr = value; }
 
     static float bestTime = 0;
+    static List<int> ownArr = new List<int>();
 
     private void Awake()
     {
@@ -35,9 +38,26 @@ public class getValueForStatistic : MonoBehaviour
     // run time start, finish, delta 
     public static void makeStatisticsTexts()
     {
+        // write into labels
         GVFS.labelComparisons.text = Comparison.ToString();
         GVFS.labelSwitches.text = Switches.ToString();
-        GVFS.labelTimeDelta.text = (StartTime - EndTime).ToString();
+        GVFS.labelTimeDelta.text = (StartTime - EndTime).ToString() + " ms";
+
+        for(byte i = 1; i<=OwnArr.Count; i++)
+        {
+            //generate pefab 
+            // increment index
+            // write in value
+        }
+        
+        if(true)
+        {
+            // try out optimal arr & get amount of switches as comparison
+        } else
+        {
+            // get worst case switch amount
+        }
+        
     }
 
 
